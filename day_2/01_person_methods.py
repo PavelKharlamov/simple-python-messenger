4e120a2adfcd559201afc9a67198a1c23d3f60b0
Pavel <p.kharlamov@intabia.ru>
2020-05-08 12:30:02 +0700

    [DAY-02] Added server & new resources.

---
+++ day_2/01_person_methods.py
@@ -0,0 +1,30 @@
+"""
+Пример программы для работы с ООП
+
+Сделать
+- добавить метод для вывода сообщений с префиксом имени
+- добавить метод для вывода информации об объекте
+- добавить конструктор класса для формирования полей
+"""
+
+
+class Person:
+    name: str
+    surname: str
+    age: int
+
+    def __init__(self, name, surname, age):
+        self.name = name
+        self.surname = surname
+        self.age = age
+
+    def info(self):
+        print(f"{self.name} {self.surname}, {self.age}")
+
+    def say_as(self, message):
+        return f"<{self.name}> {message}"
+
+
+user = Person("John", "Doe", 30)
+user.info()
+print(user.say_as("Hello!"))
